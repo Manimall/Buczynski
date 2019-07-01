52564929c9b9875f1d721c3d7354086be061d2c8
Manimall <gorpunov94@mail.ru>
2019-07-02 02:30:04 +0300

    adaptive main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // Слайдер на главной в Промо-блоке
 
+
 const swiper = new Swiper('.swiper-container', {
 	slidesPerView: 1,
 	loop: true,
@@ -8,3 +9,15 @@ const swiper = new Swiper('.swiper-container', {
 		prevEl: '.swiper-button-prev',
 	},
 });
+
+(function() {
+	const burger = document.querySelector(`.hambergerIcon`);
+	const nav = document.querySelector(`.site-nav`);
+	const body = document.querySelector(`body`);
+
+	burger.addEventListener(`click`, () => {
+		burger.classList.toggle(`open`);
+		nav.classList.toggle(`site-nav--active`);
+		body.classList.toggle(`body--overflow`);
+	});
+})();
