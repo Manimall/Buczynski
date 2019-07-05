36bc5d415d94be6fd041dc7e94ad425eaa62fbc2
Manimall <gorpunov94@mail.ru>
2019-07-05 21:14:42 +0300

    modal-form

--- src/js/main.js
+++ src/js/main.js
@@ -55,3 +55,61 @@
 		body.classList.toggle(`body--overflow`);
 	});
 })();
+
+(function() {
+	const formOrderOpenBtn = document.querySelector(`.cart__to-order`);
+	const orderPopup = document.querySelector(`.modal--buy`);
+	const orderForm = orderPopup.querySelector(`.modal__form`);
+	const formOrderCloseBtn = orderPopup.querySelector(`.modal__popup-close`);
+
+	const userName = orderForm.querySelector('#your-name');
+	const userPhone = orderForm.querySelector('#your-phone');
+
+	const userAddress = orderForm.querySelector(`#address`);
+	//добавить проверку на заполненность селекта с выбором способа оплаты
+
+	let isStorageSupport = true;
+	let storage = '';
+
+	try {
+		storage = localStorage.getItem('userName');
+	} catch (err) {
+		isStorageSupport = false;
+	}
+
+	formOrderOpenBtn.addEventListener('click', function (evt) {
+		evt.preventDefault();
+		orderPopup.classList.add('modal--open');
+
+		if (storage) {
+			userName.value = storage;
+			userPhone.focus();
+		} else {
+			userName.focus();
+		}
+	});
+
+	orderForm.addEventListener('submit', function (evt) {
+		if (!userName.value || !userPhone.value || !userAddress) {
+			evt.preventDefault();
+			orderPopup.classList.remove("modal--error");
+			orderPopup.offsetWidth = orderPopup.offsetWidth;
+			orderPopup.classList.add('modal--error');
+		} else {
+			if (isStorageSupport) {
+				localStorage.setItem(`userName`, userName.value);
+			}
+		}
+	});
+
+	const closePopup = (evt) => {
+		evt.preventDefault();
+		orderPopup.classList.remove('modal--open');
+		orderPopup.classList.remove('modal--error');
+	};
+
+	formOrderCloseBtn.addEventListener('click', closePopup);
+	document.addEventListener('keydown', function(evt) {
+		return (evt.which === 27) && closePopup(evt);
+	});
+})();
